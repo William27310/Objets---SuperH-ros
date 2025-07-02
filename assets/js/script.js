d61b2fcbc9ce38dbaac88e5268fe67e2e0151c83
William27310 <william.27@free.fr>
2025-07-02 14:58:48 +0200

    srx

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+fetch('assets/json/dc.json')
+    .then((response) => response.json())
+    .then((data) => {
+        console.log(data)
+
+        data.forEach(hero => {
+            document.getElementById('conteneur').innerHTML += `
+            <div class="card mb-5 bg-dark" style="width: 24rem; height: 62rem;">
+                <h5 class="card-title mt-2 text-white"> ${hero.name} </h5>
+                <img src= ${hero.image} class="card-img-top border rounded" alt="...">
+                <div class="card-body">
+                    <ul>
+                        <li class="card-text text-white"> <b>Capacités</b> : <br> ${hero.powers}</li><br>
+                        <li class="card-text text-white"> <b>Ville</b> : ${hero.city}</li><br>
+                        <li class="card-text text-white"> <b>Première apparition</b> : ${hero.firstappearance}</li>
+                    </ul>
+                </div>
+            </div>
+            `
+
+        });
+
+    })
+
